7eff4e5e3bd27de0e4eb2424b6ee4b3481fe57b9
Kirtan <krishsunny9876@gmail.com>
2026-05-02 15:07:57 +0530

    basename Save

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App'
 import './assets/style.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename='/Bobsun-Milk-Tracker'>
     <App />
   </BrowserRouter>
 )
